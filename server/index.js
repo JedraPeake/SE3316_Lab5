cabe45129c33ef7b1d16b9b77646b9cab464c8f5
Jedra Peake <jpeake2@uwo.ca>
2017-11-29 03:16:33 +0000

    got mongodb working

--- server/index.js
+++ server/index.js
@@ -4,6 +4,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+var port = 8081;
+//var mongoose = require('mongoose');
+
+//mongoose.connect('mongodb://localhost:27017/nasa-webApp')
+mongoose.connect(config.database);
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
+});
 
 const index = express();
 
@@ -19,7 +35,8 @@ var port = process.env.PORT || 8080;
 index.use(cors());
 
 // Set Static Folder
-index.use(express.static(path.join(__dirname, 'public')));
+//index.use(express.static(path.join(__dirname, 'public')));
+index.use(express.static(path.join(__dirname, '/../nasa-webApp/dist')));
 
 index.use('/users', users);
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,23 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 //User Register route
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
  res.send('Register');
 });
 
 //User auth route
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
  res.send('Auth');
 });
 
 //User profile route
 router.get('/profile', (req, res, next) => {
  res.send('profile');
-});
-
-//User validate route
-router.get('/validate', (req, res, next) => {
- res.send('validate');
 });
 
 module.exports = router;
