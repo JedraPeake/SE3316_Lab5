5dc813b485e45a16b0a39c425d8b4a6e4fcab11a
JedraPeake <jpeake2@uwo.ca>
2020-07-17 18:08:51 -0400

    found it missed router module export

--- nasa-web-app/src/app/core/core.module.ts
+++ nasa-web-app/src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { CoreRoutingModule } from './core-routing.module';
@@ -10,6 +11,10 @@ import { CoreRoutingModule } from './core-routing.module';
 		CommonModule,
 		SharedModule,
 		CoreRoutingModule
+	],
+	exports: [
+		RouterModule
 	]
 })
+
 export class CoreModule { }
